c04081ffbd506b0b852d16335ac6f054b5ccc763
Taylor Ripke <taylorripkecmu@gmail.com>
2017-11-15 15:15:02 -0500

    Adding MHIs and more videos

---
+++ CV_Project/Taylor's Code/Code.m
@@ -0,0 +1,58 @@
+%@Author: Taylor Ripke
+
+%------------------------
+%Create MHI image
+colormap(gray);
+fprintf('\nMotion History Image\n');
+MHIImage = zeros(360,640);
+
+counter = 1;
+notI=2;
+k=1;
+T = 10;
+v = VideoReader('c00.mp4');
+mov = struct('cdata',zeros(360,640,1,'uint8'),...
+    'colormap',[]);
+
+while hasFrame(v)
+    video = readFrame(v);
+    video = rgb2gray(video);
+    video = imresize(video,0.5);
+    Im(:,:,counter) = video;
+    counter = counter + 1;
+end
+
+rows = size(Im(:,:,1), 1);
+columns = size(Im(:,:,1),2);
+
+for i = 2:counter-1
+    for num_cols = 1:columns
+        for num_rows = 1:rows
+            if abs(Im(num_rows,num_cols,i)-Im(num_rows,num_cols,i-1)) > T
+                Im(num_rows, num_cols,i-1) = 1;
+            else
+                Im(num_rows, num_cols,i-1) = 0;
+            end
+        end
+    end
+end
+
+for i=2:counter-2
+    %figure(); imagesc(video); pause(0.001);
+    for num_cols = 1:columns
+        for num_rows = 1:rows
+            if (Im(num_rows,num_cols,i) ~= 0)
+                MHIImage(num_rows, num_cols) = i;
+            end
+        
+        end
+    end
+    %imagesc(MHIImage);
+   %pause(0.15);
+    %MHIImage=(Im(notI)~=0)*i;
+    %notI = notI + 1;
+end
+
+MHIImage = max(0,(MHIImage-1)/counter);
+imagesc(MHIImage);
+imwrite(MHIImage,'c00.png');
---
+++ CV_Project/Taylor's Code/ComputeAverageMHIs.m
@@ -0,0 +1,23 @@
+%@Author: Taylor Ripke
+
+%------------------------
+%Create Average MHI images
+colormap(gray);
+for i=1:6
+    filename = sprintf('a%02d.png', i-1);
+    img = double(imread(filename));
+    Im(:,:,i) = img;
+end
+avg = zeros(360,640);
+for x=1:6
+    for i = 1:360
+        for j=1:640
+            avg(i,j) = avg(i,j) + Im(i,j,x);
+        end
+    end
+end
+%avg = avg./6;
+avg = max(0,(avg-1)/6);
+imagesc(avg);
+avg = uint8(avg);
+imwrite(avg,'aAVG.png');
